901487da38a3aa5347c5ab439c886f5e8282bf63
janmesh Singh <Janmesh.js@gmail.com>
2023-03-02 16:58:39 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Janmesh Singh on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
